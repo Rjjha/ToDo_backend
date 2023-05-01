40afca62c3b768e08b5a7948ae14f091e0a79a34
Rjjha <96972257+Rjjha@users.noreply.github.com>
2023-05-01 09:41:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,4 +51,4 @@ app.get('/todo/complete/:id', async(req,res) => {
     res.json(todo);
 })
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+app.listen(Process.env.PORT||3001, () => console.log("Server started on port 3001"));
